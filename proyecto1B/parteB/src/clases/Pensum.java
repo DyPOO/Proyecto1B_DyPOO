50ab6e406fbbf3cdcb828b45d6f5a0cde47e5c0a
German Rojas <g.rojasc@uniandes.edu.co>
2021-04-01 13:57:11 -0500

    Creación clase Pensum

---
+++ proyecto1B/parteB/src/clases/Pensum.java
@@ -0,0 +1,11 @@
+package clases;
+
+import java.util.ArrayList;
+
+public class Pensum {
+	private String creditos;
+	private int cantidadMaterias;
+	private String requisitosAdicionales;
+	private ArrayList<Curso> cursos;
+	// private *tipo* cronograma;
+}
